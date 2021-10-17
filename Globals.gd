ab91b17730e4ed4bdf4e6ad768b76b14dde30c84
Gernot Raudner <git@gernotraudner.at>
2021-10-18 01:57:41 +0200

    tutorial

--- Globals.gd
+++ Globals.gd
@@ -1,11 +1,11 @@
 extends Node
 
 const COLOR_MAP = {
-	0: Color.white,
-	1: Color.cyan,
-	2: Color.magenta,
-	3: Color.yellow,
-	4: Color.greenyellow,
+	0: Color8(165, 53, 53),
+	1: Color8(39, 100, 166),
+	2: Color8(90, 187, 59),
+	3: Color8(164, 52, 242),
+	4: Color8(243, 219, 73),
 }
 
 const SIZE_MAP = {
@@ -16,8 +16,8 @@ const SIZE_MAP = {
 
 const SPEED_MAP = {
 	0: 1.0,
-	1: 1.8,
-	2: 2.6,
+	1: 2.0,
+	2: 3.0,
 }
 
 const SCALES_MAP = {

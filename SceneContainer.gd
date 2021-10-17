33cd503703d159271f52de98d0564adfb8fe7529
Gernot Raudner <git@gernotraudner.at>
2021-10-17 22:11:36 +0200

    alien baby

--- SceneContainer.gd
+++ SceneContainer.gd
@@ -52,6 +52,7 @@ func store_pickables() -> void:
 	for pickable in _ufo_content:
 		if not pickable:
 			_ufo_content.erase(pickable)
+			return
 		pickable.get_parent().remove_child(pickable)
 		add_child(pickable)
 
